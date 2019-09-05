3d490b62a3a2b625418fc09851a2767755d0e72f
michinobu.sekikawa <abc_0615@icloud.com>
2019-09-05 19:46:26 +0900

    lesson7.2_CRUDのための残り3つの補助ページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,13 @@ Rails.application.routes.draw do
    post 'messages', to: 'messages#create'
    put 'messages/:id', to: 'messages#update'
    delete 'messages/:id', to: 'messages#destroy'
+   
+   # index: show の補助ページ
+   get 'messages', to: 'messages#index'
+
+   # new: 新規作成用のフォームページ
+   get 'messages/new', to: 'messages#new'
+   
+   # edit: 更新用のフォームページ
+   get 'messages/:id/edit', to: 'messages#edit'
 end
