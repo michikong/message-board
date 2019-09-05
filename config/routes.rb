5d16fa55904b8234dcc413bbba16c6cf76cde657
michinobu.sekikawa <abc_0615@icloud.com>
2019-09-05 19:51:25 +0900

    lesson7.3_7つの基本ルーティングの省略形

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get 'messages/:id', to: 'messages#show'
-   post 'messages', to: 'messages#create'
-   put 'messages/:id', to: 'messages#update'
-   delete 'messages/:id', to: 'messages#destroy'
+# Rails.application.routes.draw do
+#    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#    # CRUD
+#    get 'messages/:id', to: 'messages#show'
+#    post 'messages', to: 'messages#create'
+#    put 'messages/:id', to: 'messages#update'
+#    delete 'messages/:id', to: 'messages#destroy'
    
-   # index: show の補助ページ
-   get 'messages', to: 'messages#index'
+#    # index: show の補助ページ
+#    get 'messages', to: 'messages#index'
 
-   # new: 新規作成用のフォームページ
-   get 'messages/new', to: 'messages#new'
+#    # new: 新規作成用のフォームページ
+#    get 'messages/new', to: 'messages#new'
    
-   # edit: 更新用のフォームページ
-   get 'messages/:id/edit', to: 'messages#edit'
+#    # edit: 更新用のフォームページ
+#    get 'messages/:id/edit', to: 'messages#edit'
+# end
+
+# 上記と全く同じ意味
+Rails.application.routes.draw do
+  resources :messages
 end
