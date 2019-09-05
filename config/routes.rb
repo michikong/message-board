dcbf0af861cdb7b92af12894ea58447b9c194062
michinobu.sekikawa <abc_0615@icloud.com>
2019-09-05 19:37:37 +0900

    lesson7.1_リソースに対する CRUD のための4つのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   get 'messages/:id', to: 'messages#show'
+   post 'messages', to: 'messages#create'
+   put 'messages/:id', to: 'messages#update'
+   delete 'messages/:id', to: 'messages#destroy'
 end
