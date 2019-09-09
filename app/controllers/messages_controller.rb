3e5096016e4cffca6e028c85becf35cf78c961f8
michinobu.sekikawa <abc_0615@icloud.com>
2019-09-09 21:35:49 +0900

    lesson8.9_messages#update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,6 +28,15 @@ class MessagesController < ApplicationController
   end
 
   def update
+    @message = Message.find(params[:id])
+
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
