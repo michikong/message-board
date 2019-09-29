c2ed970e5f3c08355320680423267ab8feba6682
michinobu.sekikawa <abc_0615@icloud.com>
2019-09-29 17:21:27 +0900

    プライベートのメソッドを作成し、共有化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,10 @@
 class MessagesController < ApplicationController
   def index
-     @messages = Message.all
+    @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
+    set_message 
   end
 
   def new
@@ -24,11 +24,11 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
+    set_message
   end
 
   def update
-    @message = Message.find(params[:id])
+    set_message
 
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
@@ -40,7 +40,7 @@ class MessagesController < ApplicationController
   end
 
   def destroy
-    @message = Message.find(params[:id])
+    set_message
     @message.destroy
 
     flash[:success] = 'Message は正常に削除されました'
@@ -51,6 +51,11 @@ end
 
   private
 
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
   # Strong Parameter
   def message_params
     params.require(:message).permit(:content)
